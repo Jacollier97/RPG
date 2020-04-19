6c6d235022eb17bdcf81e4978e5fe735a13855e7
AAspCodes <asp.anthony1@gmail.com>
2020-04-19 08:29:55 -0700

    add place holder methods in Enemy

--- RPG/enemy.py
+++ RPG/enemy.py
@@ -11,3 +11,22 @@ class Enemy:
         self.defence = defence
         self.magic = magic
         self.archery = archery
+
+    def do_attack(self,attack_move="basic"):
+        # place holder
+        # named do_attack because of conflict with attack variable
+        pass
+
+    def do_defence(self,defence_move="basic"):
+        # named do_defence because of conflict with defence variable
+        pass
+
+    def die(self):
+        self._produce_loot()
+        pass
+
+    def _produce_loot(self):
+        pass
+
+    def speak(self):
+        pass
