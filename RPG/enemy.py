2e8c9aea48c58251c6f6d27a97d864c1bf5a36b7
AAspCodes <asp.anthony1@gmail.com>
2020-04-19 08:25:21 -0700

    move Enemy class to enemy.py

---
+++ RPG/enemy.py
@@ -0,0 +1,13 @@
+
+
+class Enemy:
+    def __init__(self, name=None, race=None, health=None, attack=None, strength=None, defence=None, magic=None,
+                 archery=None):
+        self.name = name
+        self.race = race
+        self.health = health
+        self.attack = attack
+        self.strength = strength
+        self.defence = defence
+        self.magic = magic
+        self.archery = archery
--- RPG/main.py
+++ RPG/main.py
@@ -19,19 +19,6 @@ def getOppData():
     return data
 
 
-class Enemy:
-    def __init__(self, name=None, race=None, health=None, attack=None, strength=None, defence=None, magic=None,
-                 archery=None):
-        self.name = name
-        self.race = race
-        self.health = health
-        self.attack = attack
-        self.strength = strength
-        self.defence = defence
-        self.magic = magic
-        self.archery = archery
-
-
 data = getOppData()
 
 stats = Enemy(**data["guard"])
